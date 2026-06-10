0d8ff17dad5a5fa64ec0b057c78c233c5edf4904
YH949494 <teohyaohui@gmail.com>
2026-06-10 08:41:14 +0000

    Fix Alembic merge migration down revision

--- backend/open_webui/migrations/versions/merge_workspace_obsolete_heads.py
+++ backend/open_webui/migrations/versions/merge_workspace_obsolete_heads.py
@@ -5,15 +5,10 @@ Revises: 461111b60977, w002_workspace_folder_defaults
 Create Date: 2026-06-10
 """
 
-from typing import Sequence, Union
-
-revision: str = "merge_workspace_obsolete_heads"
-down_revision: Union[str, tuple[str, str], None] = (
-    "461111b60977",
-    "w002_workspace_folder_defaults",
-)
-branch_labels: Union[str, Sequence[str], None] = None
-depends_on: Union[str, Sequence[str], None] = None
+revision = "merge_workspace_obsolete_heads"
+down_revision = ("461111b60977", "w002_workspace_folder_defaults")
+branch_labels = None
+depends_on = None
 
 
 def upgrade() -> None:
